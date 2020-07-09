00e69b770d8f5c1e71a59522d73586864c235a45
Alexey Moiseenko <mr.wayne4321@yandex.ru>
2020-07-09 19:52:46 +0300

    fix: добавить комментарии в js файле

--- script/index.js
+++ script/index.js
@@ -1,3 +1,4 @@
+//Получаем небходимые DOM элементы
 const page = document.querySelector('.page');
 const popup = page.querySelector('.popup');
 const profileEdit = page.querySelector('.profile__edit');
@@ -8,19 +9,24 @@ const inputJob = page.querySelector('.edit-form__field-text[name=profile-job]');
 const profileName = page.querySelector('.profile__name');
 const profileAboutSelf = page.querySelector('.profile__about-self');
 
+//Функция обработчик открытия модального окна
 function toggleForm() {
+  //Если модальное окно не открыто, тогда открыть и записать данные в поля формы
   if(!popup.classList.contains('popup_visible')) {
     popup.classList.toggle('popup_visible');
     inputName.value = profileName.textContent;
     inputJob.value = profileAboutSelf.textContent;
   }
+  //Иначе просто закрыть форму
   else {
     popup.classList.toggle('popup_visible');
   }
 }
 
 function formSubmitHandler (evt) {
+  //Отменяем перезагрузку страницы после закрытия и сохранения формы
   evt.preventDefault();
+  //Сохраняем новые значения полей в профиле
   profileName.textContent = inputName.value;
   profileAboutSelf.textContent = inputJob.value;
   toggleForm();
