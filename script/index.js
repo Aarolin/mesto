759ccd6e4eb58e13c3c5a8130d633f83087cd8e5
Моисеенко А.С <moiseenko_as@hydromash.local>
2020-07-09 15:19:11 +0400

    fix: исправить ошибки, присланные ревьюером

--- script/index.js
+++ script/index.js
@@ -3,27 +3,24 @@ const popup = page.querySelector('.popup');
 const profileEdit = page.querySelector('.profile__edit');
 const editForm = page.querySelector('.edit-form');
 const buttonClose = page.querySelector('.edit-form__button-close');
-let fieldsText = page.querySelectorAll('.edit-form__field-text');
+const inputName = page.querySelector('.edit-form__field-text[name=profile-name]');
+const inputJob = page.querySelector('.edit-form__field-text[name=profile-job]');
 const profileName = page.querySelector('.profile__name');
 const profileAboutSelf = page.querySelector('.profile__about-self');
 const buttonSave = page.querySelector('.edit-form__button-save');
-fieldsText[0].value = profileName.textContent;
-fieldsText[1].value = profileAboutSelf.textContent;
 
 function toggleForm() {
     popup.classList.toggle('popup_visible');
+    inputName.value = profileName.textContent;
+    inputJob.value = profileAboutSelf.textContent;
 }
-
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-}
-
-buttonSave.addEventListener('click', function () {
-    profileName.textContent = fieldsText[0].value;
-    profileAboutSelf.textContent = fieldsText[1].value;
+    profileName.textContent = inputName.value;
+    profileAboutSelf.textContent = inputJob.value;
     toggleForm();
-});
+}
 
 profileEdit.addEventListener('click', toggleForm);
 buttonClose.addEventListener('click', toggleForm);
