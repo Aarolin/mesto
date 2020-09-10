9f70a6694c4f16a84a39f5807aa6eabb4f756ab0
Aarolin <mr.wayne4321@yandex.ru>
2020-09-10 22:29:12 +0300

    refactor: подготовить файлы классов

---
+++ components/Popup.js
@@ -0,0 +1,18 @@
+export default class Popup {
+    constructor(popupSelector) {
+        this._popupSelector = popupSelector;
+        this._element = document.querySelector(popupSelector);
+    }
+    open() {
+
+    }
+    close() {
+        
+    }
+    _handleEscClose() {
+
+    }
+    setEventListener() {
+        
+    }
+}
